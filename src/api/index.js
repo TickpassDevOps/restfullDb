5a897680a6ed733cf9a8f7c3489803ae9563dc79
Mateo <mateobinance1@gmail.com>
2024-08-05 21:00:03 -0300

    se corrigio errores al deploy

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 
 const  {getinfo, postinfo, registropost} = require('../controllers/controller');
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000)
@@ -13,8 +14,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.json());
-app.use(require('./routes/router'));
 
+app.use(require('../routes/router'));
 app.get('/', getinfo);
 app.post('eventos',postinfo);
 app.post('/usuarios',registropost);
